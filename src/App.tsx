63b917c783fb8e35fa5aad7ef45e063b2bbc5711
Juan Ignacio Leiva <juanileiva17@gmail.com>
2025-05-06 22:58:18 -0300

    feat: se crea layout con search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import Items from "./pages/Items";
+
+function Layout() {
+  return (
+    <div className="h-dvh w-dvw">
+      <header>
+        <Navbar />
+      </header>
+      <main className="h-full w-full">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <main className="h-dvh w-dvw">
-      <Navbar />
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="items" element={<Items />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
